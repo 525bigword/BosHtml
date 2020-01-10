06a4fbfa0513cf9f32715a6cd05df641e3992b62
wzh <837713748@qq.com>
2020-01-10 15:54:02 +0800

    Signed-off-by: wzh <837713748@qq.com> 增加页面全局变量，增加登录拦截cookie

--- pages/js/bos.js
+++ pages/js/bos.js
@@ -1,1 +1,19 @@
 var url='http://localhost:8089/';
+// $(function(){
+// 	$.ajaxSetup({
+// 		dataType: "json",
+// 		cache: false,
+// 	    headers: {
+// 	        "Authorization": 'bos'+$.cookie('user')
+// 	    },
+// 	    xhrFields: {
+// 		    withCredentials: true
+// 	    } /* ,
+// 	    complete: function(xhr) {
+// 	        //token过期，则跳转到登录页面
+// 	        if(xhr.responseJSON.code == 401){
+// 	            parent.location.href = baseURL + 'login.html';
+// 	        }
+// 	    } */
+// 	}); 
+// })
