16417b9e9111d2beaa0ce5ad916318c4754de8ec
wzh <837713748@qq.com>
2020-02-13 19:03:10 +0800

    Signed-off-by: wzh <837713748@qq.com> 123

--- pages/js/bos.js
+++ pages/js/bos.js
@@ -39,3 +39,19 @@ function getingDate() {
 	var now = year  + getNow(month) + getNow(date) + getNow(h) + getNow(m) + getNow(s);
 	return now;
 }
+//获取系统当前时间
+function getingDatea() {
+	var myDate = new Date();
+
+	var year = myDate.getFullYear(); //获取当前年
+	var month = myDate.getMonth() + 1; //获取当前月
+	var date = myDate.getDate(); //获取当前日
+
+
+	var h = myDate.getHours(); //获取当前小时数(0-23)
+	var m = myDate.getMinutes(); //获取当前分钟数(0-59)
+	var s = myDate.getSeconds();
+
+	var now = year  +'-'+ getNow(month)+'-' + getNow(date)+' ' + getNow(h)+':' + getNow(m)+':' + getNow(s);
+	return now;
+}
